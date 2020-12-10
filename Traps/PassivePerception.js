d110e59c8f0a365a2de14effa53851c76a405d2e
dweatherman <dweatherman@gmail.com>
2020-12-10 10:25:26 -0600

    Update PassivePerception.js

--- Traps/PassivePerception.js
+++ Traps/PassivePerception.js
@@ -1,28 +1,18 @@
-//DanW
-//Called from Multi Level Tokens...
-//arguments example = "A trap opens up!","DC15 Dex, 1d6 bludgeoning"
-//args[0]=message to player
-//args[1]=whisper to gm
-//console.log(region);
-let trapToken = canvas.drawings.placeables.find(t=>t.id === region.data.id)
-if(trapToken)
-{
-  //console.log("found trap");
-  trapToken.update({"hidden" : false});
-  let chatData = {
+//DanW  
+//Called from Multi Level Tokens... 
+//passes in token and args
+console.log(region);
+if(token.actor.data.type === "character" || token.actor.getUsers() !== []) {
+    if(actor.data.data.skills.prc.passive >= args[0]) {
+      let chatData = {
            user: game.user._id,
            speaker: ChatMessage.getSpeaker(),
-           content: args[0]
-  };
-  ChatMessage.create(chatData, {});
-  
-  let privateChat = {
-    user: game.user._id,
-	//speaker: ChatMessage.getSpeaker(),
-	whisper : ChatMessage.getWhisperRecipients("GM"),
-	content: args[1]
-  };
-  ChatMessage.create(privateChat, {});
-  trapToken.update({flags: {"multilevel-tokens": {disabled: !trapToken.data.flags["multilevel-tokens"].disabled}}});
-  
+           content: args[1]
+      };
+      ChatMessage.create(chatData, {});
+	  let trapToken = canvas.drawings.placeables.find(t=>t.id === region.data._id);
+	  console.log(trapToken);
+	  trapToken.update({flags: {"multilevel-tokens": {disabled: !trapToken.data.flags["multilevel-tokens"].disabled}}});
+      AudioHelper.play({src: "audio/Sounds/navi-hey-listen.mp3", volume: 0.8, autoplay: true, loop: false}, true);
+   }
 }
